20ae3b9888b8ba7e41f0a33dffe1b692f3c17819
raidelacruz <ryuxsein@gmail.com>
2026-07-17 15:25:45 -0500

    estructura_Backend_Wey

---
+++ proyectobackend/src/main/java/com/example/proyectobackend/ProyectobackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.proyectobackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectobackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectobackendApplication.class, args);
+	}
+
+}
---
+++ proyectobackend/src/test/java/com/example/proyectobackend/ProyectobackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.proyectobackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectobackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
